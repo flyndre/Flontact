ebfd11b75ab0ead470eb7ab4cf6303a305d6b2b5
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-07 17:30:21 +0200

    added test with degree

--- flontactTests/Services/ParserServiceTests.cs
+++ flontactTests/Services/ParserServiceTests.cs
@@ -40,5 +40,36 @@ namespace flontact.Services.Tests
             Assert.IsNotNull(contactString);
             Assert.AreEqual("Sehr geehrte Frau Sandra Berger", contactString);
         }
+
+        //Herr Dr. Sandro Gutmensch
+        [TestMethod()]
+        public void testHerrSandroGutmenschParser()
+        {
+            ParserService parser = new ParserService();
+            List<ContactPart> retValues = (List<ContactPart>)parser.Parse("Herr Dr. Sandro Gutmensch");
+
+            Assert.AreEqual(4, retValues.Count);
+            Assert.AreEqual("Herr", retValues[0].Text);
+            Assert.AreEqual("Dr.", retValues[1].Text);
+            Assert.AreEqual("Sandro", retValues[2].Text);
+            Assert.AreEqual("Gutmensch", retValues[3].Text);
+            Assert.AreEqual(ContactPartTag.Title, retValues[0].Tag);
+            Assert.AreEqual(ContactPartTag.Degree, retValues[1].Tag);
+            Assert.AreEqual(ContactPartTag.Firstname, retValues[2].Tag);
+            Assert.AreEqual(ContactPartTag.Name, retValues[3].Tag);
+
+            var contact = parser.ToContact(retValues);
+
+            Assert.IsNotNull(contact);
+            Assert.AreEqual(Gender.Male, contact.Gender);
+            Assert.AreEqual(1, contact.Degrees.Count);
+            Assert.AreEqual(1, contact.FirstNames.Count);
+            Assert.AreEqual(1, contact.LastNames.Count);
+
+            var contactString = parser.ToString(contact);
+
+            Assert.IsNotNull(contactString);
+            Assert.AreEqual("Sehr geehrter Herr Dr. Sandro Gutmensch", contactString);
+        }
     }
 }
