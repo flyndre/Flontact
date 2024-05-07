397db00cc5a0b1eb4d74b3d185302b272e1c7eaf
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-07 11:01:06 +0200

    initial test project

---
+++ FlontactTest/Services/ParserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlontactTest.Services
+{
+    [TestClass]
+    public class ParserService
+    {
+        //Frau Sandra Berger
+        [TestMethod]
+        public void testFrauSandraBergerParser()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
