2a04c444405754fffde0f7ac9c652abef59ef446
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2024-05-07 16:35:17 +0200

    fixed problem with wrong msunittest version

---
+++ flontactTests/Services/ParserServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flontactTests.Services
+{
+    internal class ParserServiceTests
+    {
+    }
+}
