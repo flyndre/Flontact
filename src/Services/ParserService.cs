e37ad7e320218de8502ed09500805fe54a9acb3f
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2024-05-10 10:39:33 +0200

    add prefixes to output string

--- src/Services/ParserService.cs
+++ src/Services/ParserService.cs
@@ -105,7 +105,7 @@ namespace flontact.Services
                         contact.Gender = gender;
                         break;
                     case ContactPartTag.Prefix:
-                        contact.Prefix.Add(part);
+                        contact.Prefixs.Add(part);
                         break;
                 }
             });
@@ -139,6 +139,8 @@ namespace flontact.Services
             returnString.AppendJoin(' ', contact.Degrees.Select(x => x.Text));
             returnString.Append(' ');
             returnString.AppendJoin(' ', contact.FirstNames.Select(x => x.Text));
+            returnString.Append(' ');
+            returnString.AppendJoin(' ', contact.Prefixs.Select(x => x.Text));
             returnString.Append(' ');
             returnString.AppendJoin('-', contact.LastNames.Select(x => x.Text));
             return returnString.ToString();
